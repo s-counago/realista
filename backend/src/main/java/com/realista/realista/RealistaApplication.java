e2b612cd351edc5a402fd6168175581d2a326614
s-counago <s.counago00@gmail.com>
2025-10-22 20:41:16 +0200

    fake batman

---
+++ backend/src/main/java/com/realista/realista/RealistaApplication.java
@@ -0,0 +1,13 @@
+package com.realista.realista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealistaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/realista/realista/RealistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realista.realista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
